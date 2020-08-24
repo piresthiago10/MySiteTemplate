5fe3d4b23daa0970adb54a460589b6ac5ce92680
Thiago Pires <thiago.pires@sectrans.com.br>
2020-08-24 17:23:14 -0300

    mudanças nos containers

--- mysitetemplate/src/app/components/navbar/navbar.component.ts
+++ mysitetemplate/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,13 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public innerWidth: any;
 
   constructor() {
-    
-   }
+
+  }
 
   ngOnInit() {
-    
+    this.innerWidth = window.innerWidth;
+  }
+
+
+  @ViewChild('nav', { static: true }) myNav: ElementRef;
+
+  @HostListener('window:scroll') onWindowScroll() {
+    if (window.scrollY > 1.5) {
+      this.myNav.nativeElement.style.backgroundColor = '#343a40';
+    } else {
+      this.myNav.nativeElement.style.backgroundColor = 'transparent';
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+    if(this.innerWidth < 990){
+      this.myNav.nativeElement.style.backgroundColor = '#343a40';
+    }
   }
 
 }
